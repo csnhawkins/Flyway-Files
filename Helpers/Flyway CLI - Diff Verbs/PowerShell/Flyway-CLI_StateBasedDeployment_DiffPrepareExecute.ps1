7fc45ca6d616ce02ccc8569eaef5d1843590bab1
chris.hawkins@red-gate.com <chris.hawkins@red-gate.com>
2024-10-17 14:33:20 +0100

    Added new parameters for Prepare and new verb Deploy

--- Helpers/Flyway CLI - Diff Verbs/PowerShell/Flyway-CLI_StateBasedDeployment_DiffPrepareExecute.ps1
+++ Helpers/Flyway CLI - Diff Verbs/PowerShell/Flyway-CLI_StateBasedDeployment_DiffPrepareExecute.ps1
@@ -9,53 +9,64 @@ $TARGET_DATABASE_JDBC = "jdbc:sqlserver://localhost;instanceName=SQLEXPRESS;data
 $TARGET_DATABASE_USER = ""
 $TARGET_DATABASE_PASSWORD = ""
 
-$FLYWAY_LICENSE_KEY = ""
+# Flyway Authentication
+#$FLYWAY_LICENSE_KEY = "" # Deprecated License Key Method
+$FLYWAY_EMAIL = "Chris.Hawkins@red-gate.com"
+$FLYWAY_TOKEN = "zzw3XaSDtJJfIJOwwrL8cnE3l8vgOVBxjFwGxiV7dEvHNGHyqx6zt5TI02ImBHH7wsCy+sNaBZrb/Py+zRO18w=="
+
 $WORKING_DIRECTORY = "C:\Redgate\GIT\Repos\AzureDevOps\Westwind"
 $ARTIFACT_DIRECTORY = "$WORKING_DIRECTORY\Artifacts"
+$SCRIPT_FILENAME = "Flyway-$DATABASE_NAME-AutoDeploymentScript-$(get-date -f yyyyMMdd).sql"
 
-# Step 1 - Create Diff Artifact
+# Pre-Step (Optional) - Create Diff Artifact
 
-flyway diff `
-"-diff.source=$SOURCE_ENVIRONMENT" `
-"-environments.$SOURCE_ENVIRONMENT.url=$SOURCE_DATABASE_JDBC" `
-"-environments.$SOURCE_ENVIRONMENT.user=$SOURCE_DATABASE_USER" `
-"-environments.$SOURCE_ENVIRONMENT.password=$SOURCE_DATABASE_PASSWORD" `
-"-diff.target=$TARGET_ENVIRONMENT" `
-"-environments.$TARGET_ENVIRONMENT.url=$TARGET_DATABASE_JDBC" `
-"-environments.$TARGET_ENVIRONMENT.user=$TARGET_DATABASE_USER" `
-"-environments.$TARGET_ENVIRONMENT.password=$TARGET_DATABASE_PASSWORD" `
-"-diff.artifactFilename=$ARTIFACT_DIRECTORY\Flyway.$DATABASE_NAME.differences-$(get-date -f yyyyMMdd).zip" `
--outputType="" `
--licenseKey="$FLYWAY_LICENSE_KEY" `
--configFiles="$WORKING_DIRECTORY\flyway.toml" `
--schemaModelLocation="$WORKING_DIRECTORY\schema-model\" `
-| Tee-Object -Variable flywayDiffs  # Capture Flyway Diff output to variable flywayDiffs and show output in console
-
-if ($flywayDiffs -like "*No differences found*") {
-  Write-Host "No changes to generate. Exiting script gracefully."
-  exit 0  # Graceful exit
-} else {
-  Write-Host "Changes detected. Proceeding with further steps."
-}
-
-# Step 2 - Prepare Deployment Script
-
-flyway prepare `
-"-prepare.changes=" `
-"-prepare.artifactFilename=$ARTIFACT_DIRECTORY\Flyway.$DATABASE_NAME.differences-$(get-date -f yyyyMMdd).zip" `
-"-deployScript=$ARTIFACT_DIRECTORY\Flyway-$DATABASE_NAME-AutoDeploymentScript-$(get-date -f yyyyMMdd).sql"
-
-# Prepare - Can create deployment script without first needing Diff.
-# flyway prepare `
-# "-prepare.source=$SOURCE_ENVIRONMENT" `
+# flyway diff `
+# "-diff.source=$SOURCE_ENVIRONMENT" `
 # "-environments.$SOURCE_ENVIRONMENT.url=$SOURCE_DATABASE_JDBC" `
 # "-environments.$SOURCE_ENVIRONMENT.user=$SOURCE_DATABASE_USER" `
 # "-environments.$SOURCE_ENVIRONMENT.password=$SOURCE_DATABASE_PASSWORD" `
-# "-prepare.target=$TARGET_ENVIRONMENT" `
+# "-diff.target=$TARGET_ENVIRONMENT" `
 # "-environments.$TARGET_ENVIRONMENT.url=$TARGET_DATABASE_JDBC" `
 # "-environments.$TARGET_ENVIRONMENT.user=$TARGET_DATABASE_USER" `
 # "-environments.$TARGET_ENVIRONMENT.password=$TARGET_DATABASE_PASSWORD" `
-# "-deployScript=C:\Redgate\GIT\Repos\AzureDevOps\Westwind\Artifacts\Flyway-$DATABASE_NAME-AutoDeploymentScript-$(get-date -f yyyyMMdd).sql"
+# "-diff.artifactFilename=$ARTIFACT_DIRECTORY\Flyway.$DATABASE_NAME.differences-$(get-date -f yyyyMMdd).zip" `
+# -outputType="" `
+# -licenseKey="$FLYWAY_LICENSE_KEY" `
+# -configFiles="$WORKING_DIRECTORY\flyway.toml" `
+# -schemaModelLocation="$WORKING_DIRECTORY\schema-model\" `
+# -email="$FLYWAY_EMAIL" `
+# -token="$FLYWAY_TOKEN"
+# | Tee-Object -Variable flywayDiffs  # Capture Flyway Diff output to variable flywayDiffs and show output in console
+
+# if ($flywayDiffs -like "*No differences found*") {
+#   Write-Host "No changes to generate. Exiting script gracefully."
+#   exit 0  # Graceful exit
+# } else {
+#   Write-Host "Changes detected. Proceeding with further steps."
+# }
+
+# Step 2a - Prepare: Consume Diff Artifact and Create Deployment SCript
+
+# flyway prepare `
+# "-prepare.changes=" `
+# "-prepare.artifactFilename=$ARTIFACT_DIRECTORY\Flyway.$DATABASE_NAME.differences-$(get-date -f yyyyMMdd).zip" `
+# "-deployScript=$ARTIFACT_DIRECTORY\Flyway-$DATABASE_NAME-AutoDeploymentScript-$(get-date -f yyyyMMdd).sql" `
+# -email="$FLYWAY_EMAIL" `
+# -token="$FLYWAY_TOKEN"
+
+# Step 2b - Prepare: Detect differences and create deployment script
+flyway prepare `
+"-prepare.source=$SOURCE_ENVIRONMENT" `
+"-environments.$SOURCE_ENVIRONMENT.url=$SOURCE_DATABASE_JDBC" `
+"-environments.$SOURCE_ENVIRONMENT.user=$SOURCE_DATABASE_USER" `
+"-environments.$SOURCE_ENVIRONMENT.password=$SOURCE_DATABASE_PASSWORD" `
+"-prepare.target=$TARGET_ENVIRONMENT" `
+"-environments.$TARGET_ENVIRONMENT.url=$TARGET_DATABASE_JDBC" `
+"-environments.$TARGET_ENVIRONMENT.user=$TARGET_DATABASE_USER" `
+"-environments.$TARGET_ENVIRONMENT.password=$TARGET_DATABASE_PASSWORD" `
+"-prepare.scriptFilename=$ARTIFACT_DIRECTORY\$SCRIPT_FILENAME" `
+-email="$FLYWAY_EMAIL" `
+-token="$FLYWAY_TOKEN"
 
 # Step 3 - Deploy to target
 
@@ -64,4 +75,4 @@ flyway deploy `
 "-environments.$TARGET_ENVIRONMENT.url=$TARGET_DATABASE_JDBC" `
 "-environments.$TARGET_ENVIRONMENT.user=$TARGET_DATABASE_USER" `
 "-environments.$TARGET_ENVIRONMENT.password=$TARGET_DATABASE_PASSWORD" `
-"-deployScript=C:\Redgate\GIT\Repos\AzureDevOps\Westwind\Artifacts\Flyway-$DATABASE_NAME-AutoDeploymentScript-$(get-date -f yyyyMMdd).sql"
+"-deploy.scriptFilename=$ARTIFACT_DIRECTORY\$SCRIPT_FILENAME"
