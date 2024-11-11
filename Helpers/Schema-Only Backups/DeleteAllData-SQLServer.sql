bcf73139b017c10738b2a4c3d2681d6c2dfa4163
chris.hawkins@red-gate.com <chris.hawkins@red-gate.com>
2024-11-11 10:31:25 +0000

    Added print logic to indicate what is currently being deleted and how many rows are left.

--- Helpers/Schema-Only Backups/DeleteAllData-SQLServer.sql
+++ Helpers/Schema-Only Backups/DeleteAllData-SQLServer.sql
@@ -13,31 +13,53 @@ GO
 -- Disable all constraints in the database
 EXEC sp_MSforeachtable "ALTER TABLE ? NOCHECK CONSTRAINT all"
 
-declare @catalog nvarchar(250);
-declare @schema nvarchar(250);
-declare @tbl nvarchar(250);
-DECLARE i CURSOR LOCAL FAST_FORWARD FOR select
-                                        TABLE_CATALOG,
-                                        TABLE_SCHEMA,
-                                        TABLE_NAME
-                                        from INFORMATION_SCHEMA.TABLES
-                                        where
-                                        TABLE_TYPE = 'BASE TABLE'
-                                        AND TABLE_NAME != 'sysdiagrams'
-                                        AND TABLE_NAME != '__RefactorLog'
+DECLARE @catalog NVARCHAR(250);
+DECLARE @schema NVARCHAR(250);
+DECLARE @tbl NVARCHAR(250);
+DECLARE @rowCount INT;
+DECLARE @totalRows INT;
+
+DECLARE i CURSOR LOCAL FAST_FORWARD FOR
+SELECT TABLE_CATALOG, TABLE_SCHEMA, TABLE_NAME
+FROM INFORMATION_SCHEMA.TABLES
+WHERE TABLE_TYPE = 'BASE TABLE'
+AND TABLE_NAME NOT IN ('sysdiagrams', '__RefactorLog');
 
 OPEN i;
 FETCH NEXT FROM i INTO @catalog, @schema, @tbl;
+
 WHILE @@FETCH_STATUS = 0
+BEGIN
+    -- Get the total number of rows in the current table
+    DECLARE @countSQL NVARCHAR(MAX) = N'SELECT @totalRows = COUNT(*) FROM [' + @catalog + '].[' + @schema + '].[' + @tbl + '];';
+    EXEC sp_executesql @countSQL, N'@totalRows INT OUTPUT', @totalRows OUTPUT;
+    
+    PRINT 'Starting deletion for table [' + @tbl + ']. Total rows to delete: ' + CAST(@totalRows AS NVARCHAR(20));
+    
+    -- Execute deletion in batches to avoid transaction log issues (adjust batch size as needed)
+    DECLARE @batchSize INT = 100000;  -- Define batch size
+    DECLARE @deletedRows INT = 0;   -- Track total deleted rows
+    
+    WHILE 1 = 1
     BEGIN
-        DECLARE @sql NVARCHAR(MAX) = N'DELETE FROM [' + @catalog + '].[' + @schema + '].[' + @tbl + '];'
-        /* Make sure these are the commands you want to execute before executing */
-        PRINT 'Executing statement: ' + @sql
-        -- EXECUTE sp_executesql @sql
-        FETCH NEXT FROM i INTO @catalog, @schema, @tbl;
+        DECLARE @innerSQL NVARCHAR(MAX) = N'DELETE TOP (' + CAST(@batchSize AS NVARCHAR(10)) + ') FROM [' + @catalog + '].[' + @schema + '].[' + @tbl + '];';
+        EXEC sp_executesql @innerSQL;
+
+        SET @rowCount = @@ROWCOUNT;  -- Get the number of rows deleted in the last batch
+        SET @deletedRows = @deletedRows + @rowCount;  -- Accumulate the deleted row count
+        
+        IF @rowCount = 0 BREAK;  -- Exit loop if no more rows are deleted
+        
+        PRINT 'Deleted ' + CAST(@deletedRows AS NVARCHAR(10)) + ' out of ' + CAST(@totalRows AS NVARCHAR(20)) + ' rows from [' + @tbl + ']...';
     END
+    
+    PRINT 'Finished deleting data from table: [' + @tbl + ']';
+    
+    FETCH NEXT FROM i INTO @catalog, @schema, @tbl;
+END;
+
 CLOSE i;
 DEALLOCATE i;
 
 -- Re-enable all constraints again
-EXEC sp_MSforeachtable "ALTER TABLE ? WITH CHECK CHECK CONSTRAINT all"
+EXEC sp_MSforeachtable "ALTER TABLE ? WITH CHECK CHECK CONSTRAINT all";
