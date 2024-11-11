f1bb3f93873b23d01db888b37ce54fd125d6dae0
chris.hawkins@red-gate.com <chris.hawkins@red-gate.com>
2024-11-11 11:30:43 +0000

    Added logic to do a TRUNCATE of the data first if applicable, for speed. Then if not, due to constraint/refrential reasons, delete will occur.

--- Helpers/Schema-Only Backups/DeleteAllData-SQLServer.sql
+++ Helpers/Schema-Only Backups/DeleteAllData-SQLServer.sql
@@ -42,31 +42,45 @@ BEGIN
         AND t.parent_class_desc = 'OBJECT_OR_COLUMN'
     )
     BEGIN
-        PRINT 'Disabling triggers on table [' + @tbl + ']';
+        PRINT 'Disabling triggers on table [' + @schema + '].[' + @tbl + ']';
         EXEC(N'ALTER TABLE [' + @catalog + '].[' + @schema + '].[' + @tbl + '] DISABLE TRIGGER ALL');
     END
 
-    -- Get the total number of rows in the current table
-    DECLARE @countSQL NVARCHAR(MAX) = N'SELECT @totalRows = COUNT(*) FROM [' + @catalog + '].[' + @schema + '].[' + @tbl + '];';
-    EXEC sp_executesql @countSQL, N'@totalRows INT OUTPUT', @totalRows OUTPUT;
-
-    PRINT 'Starting deletion for table [' + @catalog + '].[' + @schema + '].[' + @tbl + ']. Total rows to delete: ' + CAST(@totalRows AS NVARCHAR(20));
-
-    -- Execute deletion in batches to avoid transaction log issues (adjust batch size as needed)
-    DECLARE @batchSize INT = 100000;  -- Define batch size
-    DECLARE @deletedRows INT = 0;   -- Track total deleted rows
-
-    WHILE 1 = 1
+    -- Check if the table can be truncated (no foreign key constraints referencing it)
+    IF NOT EXISTS (
+        SELECT 1
+        FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS rc
+        JOIN INFORMATION_SCHEMA.TABLE_CONSTRAINTS tc ON rc.UNIQUE_CONSTRAINT_NAME = tc.CONSTRAINT_NAME
+        WHERE tc.TABLE_SCHEMA = @schema AND tc.TABLE_NAME = @tbl
+    )
     BEGIN
-        DECLARE @innerSQL NVARCHAR(MAX) = N'DELETE TOP (' + CAST(@batchSize AS NVARCHAR(10)) + ') FROM [' + @catalog + '].[' + @schema + '].[' + @tbl + '];';
-        EXEC sp_executesql @innerSQL;
+        PRINT 'Using TRUNCATE for table [' + @schema + '].[' + @tbl + ']';
+        EXEC('TRUNCATE TABLE [' + @catalog + '].[' + @schema + '].[' + @tbl + ']');
+    END
+    ELSE
+    BEGIN
+        -- Get the total number of rows in the current table
+        DECLARE @countSQL NVARCHAR(MAX) = N'SELECT @totalRows = COUNT(*) FROM [' + @catalog + '].[' + @schema + '].[' + @tbl + '];';
+        EXEC sp_executesql @countSQL, N'@totalRows INT OUTPUT', @totalRows OUTPUT;
 
-        SET @rowCount = @@ROWCOUNT;  -- Get the number of rows deleted in the last batch
-        SET @deletedRows = @deletedRows + @rowCount;  -- Accumulate the deleted row count
+        PRINT 'Starting deletion for table [' + @catalog + '].[' + @schema + '].[' + @tbl + ']. Total rows to delete: ' + CAST(@totalRows AS NVARCHAR(20));
 
-        IF @rowCount = 0 BREAK;  -- Exit loop if no more rows are deleted
+        -- Execute deletion in batches to avoid transaction log issues
+        DECLARE @batchSize INT = 100000;  -- Define batch size
+        DECLARE @deletedRows INT = 0;   -- Track total deleted rows
 
-        PRINT 'Deleted ' + CAST(@deletedRows AS NVARCHAR(10)) + ' out of ' + CAST(@totalRows AS NVARCHAR(20)) + ' rows from [' + @tbl + ']...';
+        WHILE 1 = 1
+        BEGIN
+            DECLARE @innerSQL NVARCHAR(MAX) = N'DELETE TOP (' + CAST(@batchSize AS NVARCHAR(10)) + ') FROM [' + @catalog + '].[' + @schema + '].[' + @tbl + '];';
+            EXEC sp_executesql @innerSQL;
+
+            SET @rowCount = @@ROWCOUNT;  -- Get the number of rows deleted in the last batch
+            SET @deletedRows = @deletedRows + @rowCount;  -- Accumulate the deleted row count
+
+            IF @rowCount = 0 BREAK;  -- Exit loop if no more rows are deleted
+
+            PRINT 'Deleted ' + CAST(@deletedRows AS NVARCHAR(10)) + ' out of ' + CAST(@totalRows AS NVARCHAR(20)) + ' rows from [' + @schema + '].[' + @tbl + ']...';
+        END
     END
 
     PRINT 'Finished deleting data from table: [' + @catalog + '].[' + @schema + '].[' + @tbl + ']';
@@ -83,7 +97,7 @@ BEGIN
         AND t.parent_class_desc = 'OBJECT_OR_COLUMN'
     )
     BEGIN
-        PRINT 'Re-enabling triggers on table [' + @tbl + ']';
+        PRINT 'Re-enabling triggers on table [' + @schema + '].[' + @tbl + ']';
         EXEC(N'ALTER TABLE [' + @catalog + '].[' + @schema + '].[' + @tbl + '] ENABLE TRIGGER ALL');
     END
 
