dbec07e9d39505c1733c18789f903b48e3237721
Chris Hawkins <94678272+csnhawkins@users.noreply.github.com>
2025-01-24 15:40:56 +0000

    Added print statements

--- Helpers/Callbacks/afterClean__SQLServer.sql
+++ Helpers/Callbacks/afterClean__SQLServer.sql
@@ -1,108 +1,125 @@
 -- Support for multiple schema names; drop all table constraints
 --------------------------------------------------
-declare @name varchar(100)
-declare @table varchar(100)
-declare @schema varchar(100)
+DECLARE @name VARCHAR(100)
+DECLARE @table VARCHAR(100)
+DECLARE @schema VARCHAR(100)
 
 -- Drop all table constraints: this goes through alter table
+PRINT 'Dropping all table constraints...'
 
-declare c cursor for
-	select a.name as [constraint], b.name as [table], OBJECT_SCHEMA_NAME(b.id) as [schema]
-		from dbo.sysobjects a
-		inner join dbo.sysobjects b on a.parent_obj = b.id
-		where a.xtype in ('F','D','C','UQ') and b.xtype='U'
-open c
-fetch next from c into @name, @table, @schema
-while @@FETCH_STATUS = 0
-begin
-	exec ('alter table [' + @schema + '].[' + @table + '] drop constraint [' + @name + ']')
-	fetch next from c into @name, @table, @schema
-end
-close c
-deallocate c
-
-GO
-
-if exists (select * from dbo.sysobjects where name='TestFramework_DropAll' and xtype='P')
-	drop procedure TestFramework_DropAll
+DECLARE c CURSOR FOR
+	SELECT a.name AS [constraint], b.name AS [table], OBJECT_SCHEMA_NAME(b.id) AS [schema]
+	FROM dbo.sysobjects a
+	INNER JOIN dbo.sysobjects b ON a.parent_obj = b.id
+	WHERE a.xtype IN ('F','D','C','UQ') AND b.xtype='U'
+OPEN c
+FETCH NEXT FROM c INTO @name, @table, @schema
+WHILE @@FETCH_STATUS = 0
+BEGIN
+	PRINT 'Dropping constraint [' + @name + '] on table [' + @schema + '].[' + @table + ']...'
+	EXEC ('ALTER TABLE [' + @schema + '].[' + @table + '] DROP CONSTRAINT [' + @name + ']')
+	FETCH NEXT FROM c INTO @name, @table, @schema
+END
+CLOSE c
+DEALLOCATE c
 
 GO
 
-create procedure TestFramework_DropAll (@xtype varchar(2), @drop varchar(20))
-as
-begin
-	declare @name varchar(100), @id bigint, @schema varchar(100)
-	declare c cursor for select name, id, OBJECT_SCHEMA_NAME(id) from dbo.sysobjects where xtype=@xtype
-	open c
-	fetch next from c into @name, @id, @schema
-	while @@FETCH_STATUS = 0
-	begin
-		if @name != 'TestFramework_DropAll' and @schema != 'sys'
-			exec ('DROP ' + @drop + ' [' + @schema + '].[' + @name + ']')
-		fetch next from c into @name, @id, @schema
-	end
-	close c
-	deallocate c
-end
+IF EXISTS (SELECT * FROM dbo.sysobjects WHERE name='TestFramework_DropAll' AND xtype='P')
+BEGIN
+	PRINT 'Dropping existing procedure [TestFramework_DropAll]...'
+	DROP PROCEDURE TestFramework_DropAll
+END
+
+GO
+
+CREATE PROCEDURE TestFramework_DropAll (@xtype VARCHAR(2), @drop VARCHAR(20))
+AS
+BEGIN
+	DECLARE @name VARCHAR(100), @id BIGINT, @schema VARCHAR(100)
+	DECLARE c CURSOR FOR SELECT name, id, OBJECT_SCHEMA_NAME(id) FROM dbo.sysobjects WHERE xtype=@xtype
+	PRINT 'Starting to drop objects of type [' + @xtype + ']...'
+	OPEN c
+	FETCH NEXT FROM c INTO @name, @id, @schema
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+		IF @name != 'TestFramework_DropAll' AND @schema != 'sys'
+		BEGIN
+			PRINT 'Dropping [' + @drop + '] [' + @schema + '].[' + @name + ']...'
+			EXEC ('DROP ' + @drop + ' [' + @schema + '].[' + @name + ']')
+		END
+		FETCH NEXT FROM c INTO @name, @id, @schema
+	END
+	CLOSE c
+	DEALLOCATE c
+	PRINT 'Finished dropping objects of type [' + @xtype + ']'
+END
 
 GO
 
 -- Drop stuff in this order to avoid dependency errors
-
-exec TestFramework_DropAll 'V', 'view'
+PRINT 'Dropping views...'
+EXEC TestFramework_DropAll 'V', 'view'
 GO
-exec TestFramework_DropAll 'FN', 'function'
+PRINT 'Dropping scalar functions...'
+EXEC TestFramework_DropAll 'FN', 'function'
 GO
-exec TestFramework_DropAll 'IF', 'function'
+PRINT 'Dropping inline table-valued functions...'
+EXEC TestFramework_DropAll 'IF', 'function'
 GO
-exec TestFramework_DropAll 'TF', 'function'
+PRINT 'Dropping multi-statement table-valued functions...'
+EXEC TestFramework_DropAll 'TF', 'function'
 GO
-exec TestFramework_DropAll 'U', 'table'
+PRINT 'Dropping tables...'
+EXEC TestFramework_DropAll 'U', 'table'
 GO
-exec TestFramework_DropAll 'P', 'procedure'
-GO
-
-
--- User defined types are a special case as they are not listed in sysobjects
-
-declare c cursor for
-	select name from sys.types where is_user_defined=1
-declare @name varchar(100)
-open c
-fetch next from c into @name
-while @@FETCH_STATUS = 0
-begin
-	exec ('drop type [' + @name + ']')
-	fetch next from c into @name
-end
-close c
-deallocate c
-
+PRINT 'Dropping procedures...'
+EXEC TestFramework_DropAll 'P', 'procedure'
 GO
 
+-- User-defined types are a special case as they are not listed in sysobjects
+PRINT 'Dropping user-defined types...'
+DECLARE c CURSOR FOR
+	SELECT name FROM sys.types WHERE is_user_defined=1
+DECLARE @name VARCHAR(100)
+OPEN c
+FETCH NEXT FROM c INTO @name
+WHILE @@FETCH_STATUS = 0
+BEGIN
+	PRINT 'Dropping type [' + @name + ']...'
+	EXEC ('DROP TYPE [' + @name + ']')
+	FETCH NEXT FROM c INTO @name
+END
+CLOSE c
+DEALLOCATE c
+
+GO
 
 -- Drop schemas except for standard/built-in ones
+PRINT 'Dropping non-standard schemas...'
+DECLARE c CURSOR FOR
+	SELECT name FROM sys.schemas WHERE name NOT IN ('dbo','guest','INFORMATION_SCHEMA','sys')
+DECLARE @name VARCHAR(100)
+OPEN c
+FETCH NEXT FROM c INTO @name
+WHILE @@FETCH_STATUS = 0
+BEGIN
+	PRINT 'Dropping schema [' + @name + ']...'
+	EXEC ('DROP SCHEMA [' + @name + ']')
+	FETCH NEXT FROM c INTO @name
+END
+CLOSE c
+DEALLOCATE c
 
-declare c cursor for
-	select name from sys.schemas where name not in ('dbo','guest','INFORMATION_SCHEMA','sys')
-declare @name varchar(100)
-open c
-fetch next from c into @name
-while @@FETCH_STATUS = 0
-begin
-	exec ('drop schema [' + @name + ']')
-	fetch next from c into @name
-end
-close c
-deallocate c
+GO
+
+PRINT 'Dropping defaults...'
+EXEC TestFramework_DropAll 'D', 'default'
+GO
+
+PRINT 'Dropping procedure [TestFramework_DropAll]...'
+DROP PROCEDURE TestFramework_DropAll
 
 GO
 
-
-exec TestFramework_DropAll 'D', 'default'
-GO
-
-
-drop procedure TestFramework_DropAll
-
-GO
+PRINT 'All non-standard objects and schemas have been dropped.'
